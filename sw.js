f32592a27a0dcb389392296c7bb208dbee2fcaad
oo0oo0oyj <jinyang@Jins-MacBook-Air.local>
2025-06-25 10:31:01 +0800

    Fix caching issues and force new version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'car-race-v1';
-const ASSETS = [
+const CACHE_NAME = 'car-race-v2';
+const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
@@ -9,16 +9,51 @@ const ASSETS = [
     '/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
+    // Force the waiting service worker to become the active service worker
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    // Take control of all pages immediately
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then((response) => {
+                // Clone the response because we're going to use it in two places
+                const responseToCache = response.clone();
+                
+                // Update the cache with the new version
+                caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                // If fetch fails, return from cache
+                return caches.match(event.request);
+            })
     );
 }); 
